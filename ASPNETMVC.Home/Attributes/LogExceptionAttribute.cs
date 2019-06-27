0987ff71955b45fd4eb45d54806f56f1637b228a
playitouch@163.com <playitouch@163.com>
2019-06-28 00:02:28 +0800

    提交

---
+++ ASPNETMVC.Home/Attributes/LogExceptionAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCore.Web.Attributes
+{
+    public class LogExceptionAttribute : ActionFilterAttribute
+    {
+        
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            //context.HttpContext.Response.Write();
+            base.OnActionExecuted(context);
+        }
+
+
+    }
+}
