2734273c8575659f324b457cf183967a3a7beb72
playitouch@163.com <playitouch@163.com>
2019-06-28 00:03:20 +0800

    提交

---
+++ ASPNETMVC.Home/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MvcCore.Web.Application;
+using MvcCore.Web.Common;
+
+namespace MvcCore.Web.Controllers
+{
+    public class UserController : WebController<IUserService>
+    {
+        public IActionResult Index()
+        {
+            var list = this.Service.GetList();
+            this.ViewBag.UserList = list;
+            return View();
+        }
+    }
+}
